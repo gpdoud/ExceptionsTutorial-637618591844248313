fc285d0de199f439ed8d6cee8479e267fcb0e73b
Greg Doud <gpdoud@gmail.com>
2021-07-14 11:33:05 -0400

    Committed by GitUtil

---
+++ ExceptionsTutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionsTutorial {
+    class Program {
+        static void Main(string[] args) {
+            do {
+                var nbr = Convert.ToInt32(Console.ReadLine());
+                if (nbr % 2 == 1) throw new OddNumberException(nbr);
+            } while (Console.ReadLine().ToLower() != "y");
+        }
+    }
+}
